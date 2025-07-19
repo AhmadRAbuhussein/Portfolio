5b3af5eac00f85a5689881ed9d1c7d881ad2ec92
ahmadabuhussen <ahmad@simplify9.com>
2025-07-19 20:38:24 +0300

    upgrade npm packages including react 19

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,10 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
-import logo from "../Assets/logo.png";
-import Button from "react-bootstrap/Button";
-import { Link } from "react-router-dom";
-import { CgGitFork } from "react-icons/cg";
-import { ImBlog } from "react-icons/im";
-import {
-  AiFillStar,
-  AiOutlineHome,
-  AiOutlineFundProjectionScreen,
-  AiOutlineUser,
-} from "react-icons/ai";
-
-import { CgFileDocument } from "react-icons/cg";
+import {Link} from "react-router-dom";
+import {CgFileDocument} from "react-icons/cg";
+import {AiOutlineFundProjectionScreen, AiOutlineHome, AiOutlineUser,} from "react-icons/ai";
 
 function NavBar() {
   const [expand, updateExpanded] = useState(false);
@@ -39,7 +29,7 @@ function NavBar() {
     >
       <Container>
         <Navbar.Brand href="/" className="d-flex">
-          <img src={logo} className="img-fluid logo" alt="brand" />
+          <strong className="main-name"> AA</strong>
         </Navbar.Brand>
         <Navbar.Toggle
           aria-controls="responsive-navbar-nav"
@@ -55,7 +45,7 @@ function NavBar() {
           <Nav className="ms-auto" defaultActiveKey="#home">
             <Nav.Item>
               <Nav.Link as={Link} to="/" onClick={() => updateExpanded(false)}>
-                <AiOutlineHome style={{ marginBottom: "2px" }} /> Home
+                <AiOutlineHome style={{marginBottom: "2px"}}/> Home
               </Nav.Link>
             </Nav.Item>
 
@@ -65,7 +55,7 @@ function NavBar() {
                 to="/about"
                 onClick={() => updateExpanded(false)}
               >
-                <AiOutlineUser style={{ marginBottom: "2px" }} /> About
+                <AiOutlineUser style={{marginBottom: "2px"}}/> About
               </Nav.Link>
             </Nav.Item>
 
@@ -76,7 +66,7 @@ function NavBar() {
                 onClick={() => updateExpanded(false)}
               >
                 <AiOutlineFundProjectionScreen
-                  style={{ marginBottom: "2px" }}
+                  style={{marginBottom: "2px"}}
                 />{" "}
                 Projects
               </Nav.Link>
@@ -88,7 +78,7 @@ function NavBar() {
                 to="/resume"
                 onClick={() => updateExpanded(false)}
               >
-                <CgFileDocument style={{ marginBottom: "2px" }} /> Resume
+                <CgFileDocument style={{marginBottom: "2px"}}/> Resume
               </Nav.Link>
             </Nav.Item>
 
